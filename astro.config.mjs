955fa48a6a0ccf6923d418f532622d5e1d22791e
oralrobinson21 <oralrobinson21@outlook.com>
2025-11-12 09:58:28 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://lockhabit-blog.pages.dev',
+  site: 'https://blog.lockhabit.com', // you can change later
+  output: 'static',
+  integrations: [],
 });
